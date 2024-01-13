164e1220e9119fd30beb13a7c5c56e16b6c9d097
Mahmoud Shata <mahmoudhosam23@gmail.com>
2024-01-13 15:10:10 +0200

    prepare basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgApexchartsModule } from 'ng-apexcharts';
 import { ChartsComponent } from './chart/chart.component';
+import { TableComponent } from './table/table.component';
+import { TicketComponent } from './ticket/ticket.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { QRCodeModule } from 'angularx-qrcode';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChartsComponent,
+    TableComponent,
+    TicketComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgApexchartsModule
+    NgApexchartsModule,
+    NgbModule,
+    QRCodeModule,
+    TableModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
